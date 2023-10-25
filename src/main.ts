3837f1f16927d9fddcbac29c9c022bb4483f2fa0
adimerpaul <adimer101@gmail.com>
2023-10-24 22:31:30 -0400

    colocado la parpeta public

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public'); // Asegúrate de que 'public' sea la ruta correcta a la carpeta que contiene tus imágenes
   await app.listen(3000);
 }
 bootstrap();
